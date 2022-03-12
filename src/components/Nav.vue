8868d206fb2f061fa40f9882c61705e9e1d0235b
kehan <ke_han@icloud.com>
2022-03-12 13:36:39 +0800

    添加导航栏高亮功能

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,13 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon iconName="labels" />
       标签</router-link
     >
-    <router-link to="/add" class="item">
+    <router-link to="/add" class="item" active-class="selected">
       <Icon iconName="add" /> 记账</router-link
     >
-    <router-link to="/stats" class="item">
+    <router-link to="/stats" class="item" active-class="selected">
       <Icon iconName="stats" />
       统计</router-link
     >
@@ -27,13 +27,16 @@ nav {
   flex-direction: row;
   justify-content: space-around;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-  // font-size: 12px;
+
   > .item {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     padding: 2px 0;
+  }
+  > .item.selected {
+    color: #fb7299;
   }
   .icon {
     width: 32px;
