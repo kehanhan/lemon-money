0b360114cba3e2f5bb8444b2162e0c2896ddeb07
kehan <ke_han@icloud.com>
2022-03-25 15:19:31 +0800

    让Vue监听store

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
+
+<script lang="js">
+export default {
+  data() {
+    return {
+      store: this.$store
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import "assets/style/reset";
 @import "assets/style/helper";
+
 body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
