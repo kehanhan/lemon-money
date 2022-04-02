15d90e0608ddc48e83214c74ecccb89ade65c585
kehan <ke_han@icloud.com>
2022-04-02 17:09:34 +0800

    提示用手机打开页面

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,21 @@ new Vue({
   // store,
   render: (h) => h(App),
 }).$mount("#app");
+
+window.onload = function (){
+  setTimeout(function (){
+    window.scrollTo(0,200);
+  },0)
+}
+
+if (document.documentElement.clientWidth > 500){
+  window.alert("使用手机扫码打开");
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = "50%";
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
